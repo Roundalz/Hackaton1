bf65c31109c63ad7f9d492393b3cdf21049339d3
Anghelo1010 <anghelo.pecho@ucb.edu.bo>
2025-05-05 20:24:55 -0400

    cambios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.jsx
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/Navbar';
+import Contratos from './pages/contratos/contratos';
+import Home from './pages/home';
+import Nosotros from './pages/nosotros';
+import Pagos from './pages/pago/pago';
+import Personal from './pages/personal/personal';
+import Vacaciones from './pages/vacaciones/vacaciones';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/nosotros" element={<Nosotros />} />
+        <Route path="/contratos" element={<Contratos />} />
+        <Route path="/pagos" element={<Pagos />} />
+        <Route path="/personal" element={<Personal />} />
+        <Route path="/vacaciones" element={<Vacaciones />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
